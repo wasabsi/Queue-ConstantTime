ae5046bb4528f33b956e96009ee03249888d1594
Sabrina Warner <33816901+wasabsi@users.noreply.github.com>
2017-12-23 00:10:13 -0500

    Update Stack.java

--- Stack.java
+++ Stack.java
@@ -2,18 +2,7 @@ package edu.cofc.csci230;
 
 import java.util.EmptyStackException;
 
-/**
- * Last in First Out (LIFO) Stack
- *
- * Stack interface that "closely" resembles the Stack class
- * defined in the java.util package, see link below.
- * 
- * http://docs.oracle.com/javase/7/docs/api/java/util/Stack.html
- * 
- * @author CSCI 230: Data Structures and Algorithms Fall 2017
- *
- * @param <AnyType>
- */
+
 public interface Stack<AnyType> {
 	
 	/**
@@ -26,7 +15,7 @@ public interface Stack<AnyType> {
 	/**
 	 * Removes the object at the top of the stack and returns the 
 	 * item
-	 * .
+	 * 
 	 * @return The item at the top of this stack
 	 * @throws EmptyStackException - if this stack is empty.
 	 */
